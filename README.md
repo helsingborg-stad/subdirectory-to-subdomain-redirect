a12dbb84f88fbc0122b8860194a2b5a5cf8f203a
Joel Bernerman <joel.bernerman@helsingborg.se>
2022-11-09 13:00:54 +0100

    feat: added span of entire multi network subdomains

---
+++ README.md
@@ -0,0 +1,47 @@
+<!-- SHIELDS -->
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![License][license-shield]][license-url]
+
+<p>
+  <a href="https://github.com/helsingborg-stad/subpath-to-subdomain-redirect">
+    <img src="docs/images/hbg-github-logo-combo.png" alt="Logo" width="300">
+  </a>
+</p>
+<h1>Wordpress Network Subdirectory to Subdomain redirect</h1>
+<p>
+  Redirects subdirectories to matching subdomain in wordpress networks from active sites.
+  <br />
+  <a href="https://github.com/helsingborg-stad/subpath-to-subdomain-redirect/issues">Report Bug</a>
+  ·
+  <a href="https://github.com/helsingborg-stad/subpath-to-subdomain-redirect/issues">Request Feature</a>
+</p>
+
+## Constants
+Defaults to only redirect from domain of blog id 1, can be overridden with constants with one or more blog ids;
+```php
+define('SUBDIRECTORY_TO_SUBDOMAIN_REDIRECT_ACTIVE_BLOGS', [1, 2]);
+```
+
+## License
+Distributed under the [MIT License][license-url].
+
+
+## Acknowledgements
+- [othneildrew Best README Template](https://github.com/othneildrew/Best-README-Template)
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/helsingborg-stad/subpath-to-subdomain-redirect.svg?style=flat-square
+[contributors-url]: https://github.com/helsingborg-stad/subpath-to-subdomain-redirect/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/helsingborg-stad/subpath-to-subdomain-redirect.svg?style=flat-square
+[forks-url]: https://github.com/helsingborg-stad/subpath-to-subdomain-redirect/network/members
+[stars-shield]: https://img.shields.io/github/stars/helsingborg-stad/subpath-to-subdomain-redirect.svg?style=flat-square
+[stars-url]: https://github.com/helsingborg-stad/subpath-to-subdomain-redirect/stargazers
+[issues-shield]: https://img.shields.io/github/issues/helsingborg-stad/subpath-to-subdomain-redirect.svg?style=flat-square
+[issues-url]: https://github.com/helsingborg-stad/subpath-to-subdomain-redirect/issues
+[license-shield]: https://img.shields.io/github/license/helsingborg-stad/subpath-to-subdomain-redirect.svg?style=flat-square
+[license-url]: https://raw.githubusercontent.com/helsingborg-stad/subpath-to-subdomain-redirect/main/LICENSE
---
+++ SubdirectoryToSubdomainRedirect.php
@@ -0,0 +1,98 @@
+<?php
+
+/*
+Plugin Name:    Subdirectory to subdomain redirect
+Description:    Redirects any sudirectory call that matches an existing subdomain in a network install.
+Version:        1.0.0
+Author:         Sebastian Thulin, Joel Bernerman
+*/
+
+namespace SubdirectoryToSubdomainRedirect;
+
+class SubdirectoryToSubdomainRedirect
+{
+    public $activeRedirectBlogs = [1];
+
+    public function __construct()
+    {
+        if (defined('SUBDIRECTORY_TO_SUBDOMAIN_REDIRECT_ACTIVE_BLOGS') && isarray(SUBDIRECTORY_TO_SUBDOMAIN_REDIRECT_ACTIVE_BLOGS)) {
+            $this->activeRedirectBlogs = SUBDIRECTORY_TO_SUBDOMAIN_REDIRECT_ACTIVE_BLOGS;
+        }
+
+        add_action('init', array($this, 'init'));
+    }
+
+    
+    /**
+     * Initializes the redirect process by getting the sites and stuff.
+     * @return void
+     */
+    public function init()
+    {
+        // Only redirect from active blogs;
+        if (!in_array(get_current_blog_id(), $this->$activeRedirectBlogs)) {
+            return;
+        }
+
+        $response = $this->getSites();
+
+        $response = apply_filters('siteRedirect', $response);
+
+        $this->maybeRedirect($response);
+    }
+
+    /**
+     * Redirect if needed.
+     * @param  array $sites List of sites
+     * @return bool
+     */
+    public function maybeRedirect(array $sites) : bool
+    {
+        $currentPath = trim($this->currentPath(), '/');
+
+        foreach ($sites as $site) {
+            $subdomain = explode('.', $site->domain);
+            $subdomain = reset($subdomain);
+
+            if (substr($currentPath, 0, strlen($subdomain)) != $subdomain) {
+                continue;
+            }
+
+            wp_redirect($site->url. "/" . str_ireplace($subdomain . "/", "", $currentPath . "/"), '302');
+            exit;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the current url path.
+     * @return string
+     */
+    public function currentPath() : string
+    {
+        return trailingslashit($_SERVER['REQUEST_URI']);
+    }
+
+    /**
+     * Get list of sites.
+     * @return array
+     */
+    public function getSites() : array
+    {
+
+        $response = get_sites(array('network_id' => null));
+
+        if (!$response) {
+            return array();
+        }
+
+        // Remove active redirect sites from result.
+        $response = array_filter($response, function ($site) {
+            return !in_array($site->blog_id, $this->$activeRedirectBlogs);
+        });
+        return $response;
+    }
+}
+
+new \SubdirectoryToSubdomainRedirect\SubdirectoryToSubdomainRedirect();
